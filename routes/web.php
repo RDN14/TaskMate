64d4d07f2953717432b68d1ba21d4ff78deb0460
VioAldira <165400183+VioAldira@users.noreply.github.com>
2025-05-03 07:16:26 +0700

    Update route web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+
+Route::get('/todo/create', function () {
+    return view('todo.create');
+});
